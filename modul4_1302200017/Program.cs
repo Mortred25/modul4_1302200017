0286038b7c668c4e3528eeaadc564a13eb935f36
Mortred25 <amirulgadavi@student.telkomuniversity.ac.id>
2022-03-19 14:30:45 +0700

    menambahkan class SimpleDataBase.

--- modul4_1302200017/Program.cs
+++ modul4_1302200017/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace modul4_1302200017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Penjumlahan.JumlahTigaAngka<int>(13, 20, 17));
+            SimpleDataBase<int> db = new SimpleDataBase<int>();
+            db.AddNewData(13);
+            db.AddNewData(20);
+            db.AddNewData(17);
+            db.PrintAllData();
+
+        }
+    }
+
+    class Penjumlahan
+    {
+        public static T JumlahTigaAngka<T>(T input1, T input2, T input3)
+        {
+            dynamic a = input1;
+            dynamic b = input2;
+            dynamic c = input3;
+
+            return a + b + c;
+
+        }
+    }
+
+
+    class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+        public SimpleDataBase()
+        {
+            this.storedData = new List<T>();
+            this.inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T newData)
+        {
+            this.inputDates.Add(DateTime.Now);
+            this.storedData.Add(newData);
+        }
+
+        public void PrintAllData()
+        {
+            for (int i = 0; i < this.inputDates.Count; i++)
+            {
+                Console.WriteLine("Data " + i + " berisi: " + this.storedData[i] +
+                    ", yang disimpan pada waktu UTC: " + this.inputDates[i]);
+            }
+        }
+
+    }
+}
